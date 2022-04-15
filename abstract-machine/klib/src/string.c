fac6506013fbf0c163efacea1e3176bc81b0cc22
22040413-Nie Kai <578099534@qq.com>
2022-04-15 22:04:47 +0800

    finish difftest

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -3,58 +3,113 @@
 #include <stdint.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
 size_t strlen(const char *s) {
-  //panic("Not implemented");
-  const char *sc;
-
-  for(sc = s; *sc != '\0'; ++sc)
-    ;
-
-  return sc-s;
+  size_t i = 0;
+  while(s[i] && s[i] != '\0') i++;
+  return i;
 }
 
-char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
-}
+char *strcpy(char* dst,const char* src) {
+  /* 错误的实现方法
+   * return strncpy(dst, src, strlen(dst) );
+   *
+   * 正确的实现方法
+   * return strncpy(dst, src, strlen(src));但这种方法效率比较低下
+   */
 
-char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
-}
-
-char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
-}
-
-int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
-}
-
-int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
-}
-
-void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
-}
-
-void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
-}
-
-void *memcpy(void *out, const void *in, size_t n) {
-  //panic("Not implemented");
+  size_t i;
+  for(i=0;src[i]!='\0';i++) 
+    dst[i]=src[i];
   
-   if(!(out && in)) return 0;
-   char * pchout = (char*)out;
-   char * pchin  = (char*)in;
-   while(n--) *pchout++ = *pchin++;
-   
-   return pchout;
+  dst[i]='\0';
+  return dst;
 }
 
-int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+char *strncpy(char* dst, const char* src, size_t n) {
+  size_t i;
+  for (i = 0; i < n && src[i]!= '\0'; i++) dst[i] = src[i];
+  for (; i < n; i++) dst[i] = '\0';
+  return dst;
 }
 
+char* strcat(char* dst, const char* src) {
+  
+  /* 虽然这种方法也可以通过，但是性能非常非常的低下
+   * strcpy (dst + strlen(src), src);
+   * return dst;
+   */
+  
+  char *str=dst;
+  assert(dst!=NULL && src!=NULL);
+  while(*dst!='\0') {
+    dst++;
+  }
+  while((*dst++ = *src++) != '\0');
+  return str;
+}
+
+int strcmp(const char* s1, const char* s2) {
+  // size_t s1_size = strlen(s1);
+  // size_t s2_size = strlen(s2);
+  // size_t n = s1_size < s2_size ? s1_size : s2_size;
+  // return strncmp(s1, s2, n);
+
+  uint32_t i = 0;
+  for (i = 0; s1[i] == s2[i]; i++) if(s1[i] == '\0') return 0;
+
+  return ((signed char *)s1)[i] - ((signed char *)s2)[i];
+}
+
+int strncmp(const char* s1, const char* s2, size_t n) {
+  
+  /* 虽然可以通过测试，但是性能太低了
+  // size_t s1_size = strlen(s1);
+  // size_t s2_size = strlen(s2);
+  
+  // if (s1_size < n) n = s1_size;
+  // if (s2_size < n) n = s2_size;
+  
+  // uint32_t i = 0;
+  // for (i = 0; i < n; i++) {
+  // 	int D_value = s1[i] - s2[i];
+  // 	if (D_value) return D_value;
+  // }
+  // return 0;
+  */
+  int D_value = 0;
+  size_t i    = 0;
+  
+  for (i = 0; *s1 && *s2 && i < n && D_value != 0; i++) {
+    s1++; s2++;
+  	D_value = *s1 - *s2;
+  }
+
+  return D_value;
+}
+
+void* memset(void* v,int c,size_t n) {
+  const unsigned char uc = c; /* only cat the lowest c */
+  unsigned char *vu;
+  for (vu = v; 0 < n; ++vu, --n)
+	  *vu = uc;
+  return v;
+}
+
+void* memcpy(void* out, const void* in, size_t n) {
+  const char *src = in;
+  char *dst = out;
+
+  while (n--) *dst++ = *src++;
+
+  return out;
+}
+
+int memcmp(const void* s1, const void* s2, size_t n){
+  if(!n) return 0;
+  while(--n && *(char *)s1 == *(char *)s2) {
+	s1 = (char *)s1 + 1;
+	s2 = (char *)s2 + 1;
+  }
+  return (*((unsigned char *)s1) - *((unsigned char *)s2));
+}
 #endif
--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -8,7 +8,6 @@ void init_difftest(char *ref_so_file, long img_size, int port);
 void init_device();
 void init_sdb();
 void init_disasm(const char *triple);
-void init_wp_pool();
 
 static void welcome() {
   Log("Trace: %s", MUXDEF(CONFIG_TRACE, ASNI_FMT("ON", ASNI_FG_GREEN), ASNI_FMT("OFF", ASNI_FG_RED)));
@@ -18,7 +17,7 @@ static void welcome() {
   Log("Build time: %s, %s", __TIME__, __DATE__);
   printf("Welcome to %s-NEMU!\n", ASNI_FMT(str(__GUEST_ISA__), ASNI_FG_YELLOW ASNI_BG_RED));
   printf("For help, type \"help\"\n");
-  //Log("Exercise: Please remove me in the source code and compile NEMU again.");
+  Log("Exercise: Please remove me in the source code and compile NEMU again.");
   //assert(0);
 }
 
@@ -70,7 +69,7 @@ static int parse_args(int argc, char *argv[]) {
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
-      case 1: img_file = optarg; return optind - 1;
+      case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
         printf("\t-b,--batch              run with batch mode\n");
@@ -113,9 +112,6 @@ void init_monitor(int argc, char *argv[]) {
 
   /* Initialize the simple debugger. */
   init_sdb();
-
-  /* Initialize the watchpoint pool. */
-  init_wp_pool();
 
   IFDEF(CONFIG_ITRACE, init_disasm(
     MUXDEF(CONFIG_ISA_x86,     "i686",
